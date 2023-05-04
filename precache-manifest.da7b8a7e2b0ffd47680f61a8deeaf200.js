64a9c5ff39f4071de77b2f64292d60c761ae4959
Code Sandbox <hello@codesandbox.io>
2023-05-04 22:05:18 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.da7b8a7e2b0ffd47680f61a8deeaf200.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4035f8e5191699bd51c8332fbf124865",
+    "url": "https://jakson0070.github.io/csb-rlt57t/index.html"
+  },
+  {
+    "revision": "5c3d0e492be1af96ac67",
+    "url": "https://jakson0070.github.io/csb-rlt57t/static/js/2.d320fb90.chunk.js"
+  },
+  {
+    "revision": "2478c7ce95fbfa151ccd",
+    "url": "https://jakson0070.github.io/csb-rlt57t/static/js/main.f75318d4.chunk.js"
+  },
+  {
+    "revision": "02e24c0d7e4c3863e32f",
+    "url": "https://jakson0070.github.io/csb-rlt57t/static/js/runtime-main.11e3c621.js"
+  }
+]);
